51a77df2e910d256be0a1c251ce7927148af142d
Vojtěch Kraus <vojtech.kraus@orchitech.cz>
2023-05-24 17:41:40 +0200

    Add tests for datastore authentication.

---
+++ auth-datastore/02-auth-failed.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+. "$(dirname "${BASH_SOURCE[0]}")/../.common.sh"
+. "$(dirname "${BASH_SOURCE[0]}")/../.client.sh"
+
+TEST_USER_USERNAME=amadmin
+TEST_USER_PASSWORD=wrong_password
+
+clear_cookies
+
+AUTH_RESPONSE=$(
+  authentication_request < /dev/null \
+  | assert_response_status \
+  | assert_response_body '.stage == "DataStore1"'
+)
+
+AUTH_REQUEST=$(
+  get_response_body "$AUTH_RESPONSE" \
+  | jq ".callbacks[0].input[0].value = \"$TEST_USER_USERNAME\"" \
+  | jq ".callbacks[1].input[0].value = \"$TEST_USER_PASSWORD\""
+)
+
+authentication_request <<< "$AUTH_REQUEST" \
+  | assert_response_status 401 \
+  > /dev/null
